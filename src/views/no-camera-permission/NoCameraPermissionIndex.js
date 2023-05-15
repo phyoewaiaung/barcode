e1990ecec482039f781cf8bc5cb474e2a9ab7d74
phyoewaiaung <phyoewaiaung082@gmail.com>
2023-05-15 22:18:00 +0630

    ststing

--- src/views/no-camera-permission/NoCameraPermissionIndex.js
+++ src/views/no-camera-permission/NoCameraPermissionIndex.js
@@ -5,7 +5,7 @@ const NoCameraPermissionIndex = () => {
     return (
         <div className='no-camera-access'>
             <img src='/images/camera.png' alt="barcode" width={50} height={50}/>
-            <p>Camera permission is required to use this app.Please enable camera access in your browser setting!</p>
+            <p className='ms-3 me-3'>Camera permission is required to use this app.Please enable camera access in your browser setting!</p>
         </div>
     )
 }
